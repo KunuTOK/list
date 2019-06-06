27854098b5bdf8027b4a74567d406d44555ff7d7
KunuTOK <kunutok@mail.ru>
2019-06-06 23:31:12 +0300

    Update App.js

--- list/App.js
+++ list/App.js
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View, AppRegistry, TextInput } from 'react-native';
-
-
 
  export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.a}>
-        <Text>  № | ☑ | Товара | €,₽,$ | количество | шт,упк,г,мл |</Text>
+      <View>
+      <Text>  № | ☑ | Товара | €,₽,$ | количество | шт,упк,г,мл |</Text>
+      <UselessTextInput />
+      </View>
+    );
+  }
+}
+
+ class UselessTextInput extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: 'Useless Placeholder' };
+  }
+
+  render() {
+    return (
+      <View>
+      <TextInput
+        style={{height: 200, borderColor: 'gray', borderWidth: 1}}
+        onChangeText={(text) => this.setState({text})}
+        value={this.state.text}
+      />
         </View>
     );
   }
 }
 
- const styles = StyleSheet.create({
+// skip this line if using Create React Native App
+AppRegistry.registerComponent('AwesomeProject', () => UselessTextInput);
+  const styles = StyleSheet.create({
     a: {
     flex: 0,
     backgroundColor: 'ffd700',
