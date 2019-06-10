5301e25add49b5a256c500a8c59e6795ac26a26b
KunuTOK <kunutok@mail.ru>
2019-06-11 00:50:40 +0300

    Update App.js

--- list/App.js
+++ list/App.js
@@ -11,16 +11,27 @@ import {
 export default () => {
   return (
     <View style={styles.a}>
-      <Text> № | ☑ | Товара | €,₽,$ | количество | шт,упк,г,мл |</Text>
-      <UselessTextInput />
-      <Button
-        onPress={true}
-        title="Learn More"
-        color="#841584"
-        accessibilityLabel="Learn more about this purple button"
-      />
-    </View>
-  );
+      <Text> № | ☑ | Товар | цена | количество | шт,упк,г,мл |</Text>
+        <Text
+        style={{
+          flex: 0,
+        // height: 20,
+        width: 380,
+          //borderColor: "gray",
+          alignItems: "center",
+          justifyContent: "center"
+        }}> № | ☑ | Картофель | 15 ₽ | 5 | кг |</Text>
+        <Text> № | ☑ | молоко | 36,9 ₽ | 2 | упк |</Text>
+        <Text> № | ☑ | авокадо | 60 ₽ | 3 | шт |</Text>
+        <UselessTextInput />
+        <Button
+          onPress={true}
+          title="добавить"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        />
+          <Text> итого: 328,8 ₽ </Text>
+    </View>);
 };
 
 function UselessTextInput() {
@@ -32,7 +43,7 @@ function UselessTextInput() {
           height: 30,
           borderColor: "gray",
           borderWidth: 1,
-          alignItems: "center",
+          alignItems: "stretch",
           justifyContent: "space betwen"
         }}
         onChangeText={text => setText(text)}
@@ -46,9 +57,11 @@ const styles = StyleSheet.create({
   a: {
     flex: 0,
     backgroundColor: "ffd700",
-    alignItems: "center",
+    alignItems: "stretch",
     justifyContent: "center",
-    height: 100,
-    borderWidth: 2
+//    borderWidth: 2,
+//    flexDirection: "row",
+    height: 200,
+    width: 380,
   }
 });
