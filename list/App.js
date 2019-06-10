4f803f47b3bf445f6795fdd01085b31b01eaf77b
JLarky <jlarky@gmail.com>
2019-06-10 12:26:36 -0700

    hooks

--- list/App.js
+++ list/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import {
   StyleSheet,
   Text,
@@ -8,46 +8,38 @@ import {
   Button
 } from "react-native";
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.a}>
-        <Text> № | ☑ | Товара | €,₽,$ | количество | шт,упк,г,мл |</Text>
-        <UselessTextInput />
-        <Button
-          onPress={true}
-          title="Learn More"
-          color="#841584"
-          accessibilityLabel="Learn more about this purple button"
-        />
-      </View>
-    );
-  }
-}
+export default () => {
+  return (
+    <View style={styles.a}>
+      <Text> № | ☑ | Товара | €,₽,$ | количество | шт,упк,г,мл |</Text>
+      <UselessTextInput />
+      <Button
+        onPress={true}
+        title="Learn More"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
+    </View>
+  );
+};
 
-class UselessTextInput extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { text: "введите текст" };
-  }
-
-  render() {
-    return (
-      <View>
-        <TextInput
-          style={{
-            height: 30,
-            borderColor: "gray",
-            borderWidth: 1,
-            alignItems: "center",
-            justifyContent: "space betwen"
-          }}
-          onChangeText={text => this.setState({ text })}
-          value={this.state.text}
-        />
-      </View>
-    );
-  }
+function UselessTextInput() {
+  const [text, setText] = React.useState("введите текст");
+  return (
+    <View>
+      <TextInput
+        style={{
+          height: 30,
+          borderColor: "gray",
+          borderWidth: 1,
+          alignItems: "center",
+          justifyContent: "space betwen"
+        }}
+        onChangeText={text => setText(text)}
+        value={text}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
