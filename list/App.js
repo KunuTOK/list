1a99400ffb28a365eecb81491e29c06a355d1e0a
KunuTOK <kunutok@mail.ru>
2019-06-12 23:54:26 +0300

    style

--- list/App.js
+++ list/App.js
@@ -1,41 +1,83 @@
-import React from "react";
+import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
   View,
   AppRegistry,
   TextInput,
-  Button
+  Button,
+  TouchableOpacity,
+  Alert,
 } from "react-native";
+import {
+  Table,
+  TableWrapper,
+  Row,
+  Rows,
+  Col,
+  Cell
+} from 'react-native-table-component';
 
-export default () => {
-  return (
-    <View style={styles.a}>
-      <Text> № | ☑ | Товар | цена | количество | шт,упк,г,мл |</Text>
-        <Text
-        style={{
-          flex: 0,
-        // height: 20,
-        width: 380,
-          //borderColor: "gray",
-          alignItems: "center",
-          justifyContent: "center"
-        }}> № | ☑ | Картофель | 15 ₽ | 5 | кг |</Text>
-        <Text> № | ☑ | молоко | 36,9 ₽ | 2 | упк |</Text>
-        <Text> № | ☑ | авокадо | 60 ₽ | 3 | шт |</Text>
-        <UselessTextInput />
-        <Button
-          onPress={true}
-          title="добавить"
-          color="#841584"
-          accessibilityLabel="Learn more about this purple button"
-        />
-          <Text> итого: 328,8 ₽ </Text>
-    </View>);
-};
+export default class ExampleFour extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tableHead: ['№', '☑', 'Товар', 'цена', 'количество', 'шт,упк,г,мл'],
+      tableData: [
+        ['1', '☑', 'молоко', '36,9 ₽', '2', 'упк' ],
+        ['2', '☑', 'авокадо', '60 ₽', '3', 'шт' ],
+        ['3', '☑', 'Картофель', '15 ₽ ', '5', 'кг' ],
+      ]
+    }
+  }
+
+  _alertIndex(index) {
+    Alert.alert(`отмеченный товар проведен через кассу `);
+  }
+
+  render() {
+    const state = this.state;
+    const element = (data, index) => (
+      <TouchableOpacity onPress={() => this._alertIndex(index)}>
+        <View style={styles.btn}>
+          <Text style={styles.btnText}>касса</Text>
+        </View>
+      </TouchableOpacity>
+    );
+
+    return (
+     <View style={styles.container}>
+        <Table borderStyle={{borderColor: 'transparent'}}>
+          <Row data={state.tableHead} flexArr={[0.35, 0.35, 1, 0.75, 0.75, 0.55]} style={styles.head} textStyle={styles.text}/>
+          {
+            state.tableData.map((rowData, index) => (
+              <TableWrapper style={styles.row} >
+                {
+                  rowData.map((cellData, cellIndex) => (
+                    <Cell data={cellIndex === 7 ? element(cellData, index) : cellData} flexArr={[0.35, 0.35, 1, 0.75, 0.75, 0.55]} textStyle={styles.text}/>
+                  ))
+                }
+              </TableWrapper>
+            ))
+          }
+        </Table>
+       <UselessTextInput/>
+         <View style={styles.total}>
+          <Text style={styles.totaltxt}> итого: 328,8 ₽ </Text>
+         </View>
+      <TouchableOpacity onPress={() => this._alertIndex(index)}>
+        <View style={styles.btn}>
+          <Text style={styles.btnText}>касса</Text>
+        </View>
+      </TouchableOpacity>
+      </View>
+    )
+  }
+}
+
 
 function UselessTextInput() {
-  const [text, setText] = React.useState("введите текст");
+  const [text, setText] = React.useState("например: молоко");
   return (
     <View>
       <TextInput
@@ -52,16 +94,13 @@ function UselessTextInput() {
     </View>
   );
 }
-
 const styles = StyleSheet.create({
-  a: {
-    flex: 0,
-    backgroundColor: "ffd700",
-    alignItems: "stretch",
-    justifyContent: "center",
-//    borderWidth: 2,
-//    flexDirection: "row",
-    height: 200,
-    width: 380,
-  }
+  container: { flex: 1, padding: 0, paddingTop: 20, backgroundColor: '#fff' },
+  head: { height: 30, backgroundColor: '#808B97' },
+  text: { margin: 6 },
+  row: { flexDirection: 'row', backgroundColor: '#FFF1C1' },
+  btn: { flex: 0, height: 40, backgroundColor: '#78B7BB',  borderRadius: 5},
+  btnText: { fontSize: 25, textAlign: 'center', color: '#fff' },
+  total: {flex: 0, height: 40, justifyContent: 'center', alignItems: 'flex-end', backgroundColor: 'blue'},
+  totaltxt: { fontSize: 20, textAlign: 'center', color: '#fff' },
 });
