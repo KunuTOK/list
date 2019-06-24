bfc46b2b20bcf50dcc48e65160b3e5f8bfcb2f63
KunuTOK <kunutok@mail.ru>
2019-06-24 22:13:02 +0300

    22,12

--- list/App.js
+++ list/App.js
@@ -1,106 +1,30 @@
-import React, { Component } from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  AppRegistry,
-  TextInput,
-  Button,
-  TouchableOpacity,
-  Alert,
-} from "react-native";
-import {
-  Table,
-  TableWrapper,
-  Row,
-  Rows,
-  Col,
-  Cell
-} from 'react-native-table-component';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { CheckBox } from 'react-native-elements'
 
-export default class ExampleFour extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      tableHead: ['№', '☑', 'Товар', 'цена', 'количество', 'шт,упк,г,мл'],
-      tableData: [
-        ['1', '☑', 'молоко', '36,9 ₽', '2', 'упк' ],
-        ['2', '☑', 'авокадо', '60 ₽', '3', 'шт' ],
-        ['3', '☑', 'Картофель', '15 ₽ ', '5', 'кг' ],
-      ]
-    }
-  }
 
-  _alertIndex(index) {
-    Alert.alert(`отмеченный товар проведен через кассу `);
-  }
-
+ export default class App extends React.Component {
   render() {
-    const state = this.state;
-    const element = (data, index) => (
-      <TouchableOpacity onPress={() => this._alertIndex(index)}>
-        <View style={styles.btn}>
-          <Text style={styles.btnText}>касса</Text>
-        </View>
-      </TouchableOpacity>
-    );
-
     return (
-     <View style={styles.container}>
-        <Table borderStyle={{borderColor: 'transparent'}}>
-          <Row data={state.tableHead} flexArr={[0.35, 0.35, 1, 0.75, 0.75, 0.55]} style={styles.head} textStyle={styles.text}/>
-          {
-            state.tableData.map((rowData, index) => (
-              <TableWrapper style={styles.row} >
-                {
-                  rowData.map((cellData, cellIndex) => (
-                    <Cell data={cellIndex === 7 ? element(cellData, index) : cellData} flexArr={[0.35, 0.35, 1, 0.75, 0.75, 0.55]} textStyle={styles.text}/>
-                  ))
-                }
-              </TableWrapper>
-            ))
-          }
-        </Table>
-       <UselessTextInput/>
-         <View style={styles.total}>
-          <Text style={styles.totaltxt}> итого: 328,8 ₽ </Text>
-         </View>
-      <TouchableOpacity onPress={() => this._alertIndex(index)}>
-        <View style={styles.btn}>
-          <Text style={styles.btnText}>касса</Text>
-        </View>
-      </TouchableOpacity>
+      <View style={styles.container}>
+        <Text>Hello, world!</Text>
+        <CheckBox
+  title='Click Here'
+  checked={this.state.checked}
+/>
       </View>
-    )
+
+    );
   }
 }
 
-
-function UselessTextInput() {
-  const [text, setText] = React.useState("например: молоко");
-  return (
-    <View>
-      <TextInput
-        style={{
-          height: 30,
-          borderColor: "gray",
-          borderWidth: 1,
-          alignItems: "stretch",
-          justifyContent: "space betwen"
-        }}
-        onChangeText={text => setText(text)}
-        value={text}
-      />
-    </View>
-  );
-}
-const styles = StyleSheet.create({
-  container: { flex: 1, padding: 0, paddingTop: 20, backgroundColor: '#fff' },
-  head: { height: 30, backgroundColor: '#808B97' },
-  text: { margin: 6 },
-  row: { flexDirection: 'row', backgroundColor: '#FFF1C1' },
-  btn: { flex: 0, height: 40, backgroundColor: '#78B7BB',  borderRadius: 5},
-  btnText: { fontSize: 25, textAlign: 'center', color: '#fff' },
-  total: {flex: 0, height: 40, justifyContent: 'center', alignItems: 'flex-end', backgroundColor: 'blue'},
-  totaltxt: { fontSize: 20, textAlign: 'center', color: '#fff' },
+ const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
+
+// Примечание: hello world взято у себя для проверки раотоспособности. добавлен только CheckBox вот от сюда https://react-native-training.github.io/react-native-elements/docs/checkbox.html#checkedtitle
---
+++ list/version/only visual.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  AppRegistry,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
+import {
+  Table,
+  TableWrapper,
+  Row,
+  Rows,
+  Cell
+} from 'react-native-table-component';
+
+export default class ExampleFour extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tableHead: ['№', '☑', 'Товар', 'цена', 'количество', 'шт,упк,г,мл'],
+      tableData: [
+        ['1', '☑', 'молоко', '36,9 ₽', '2', 'упк' ],
+        ['2', '☑', 'авокадо', '60 ₽', '3', 'шт' ],
+        ['3', '☑', 'Картофель', '15 ₽ ', '5', 'кг' ],
+      ]
+    }
+  }
+  render() {
+      const state = this.state;
+        <View style={styles.btn}>
+          <Text style={styles.btnText}>касса</Text>
+        </View>
+
+
+    return (
+     <View style={styles.container}>
+        <Table borderStyle={{borderColor: 'transparent'}}>
+          <Row data={state.tableHead} flexArr={[0.35, 0.35, 1, 0.75, 0.75, 0.55]} style={styles.head} textStyle={styles.text}/>
+              <TableWrapper style={styles.row} >
+          <Rows data={state.tableData} flexArr={[0.35, 0.35, 1, 0.75, 0.75, 0.55]} style={styles.row} textStyle={styles.text}/>
+              </TableWrapper>
+        </Table>
+       <UselessTextInput/>
+         <View style={styles.total}>
+          <Text style={styles.totaltxt}> итого: 328,8 ₽ </Text>
+         </View>
+        <View style={styles.btn}>
+          <Text style={styles.btnText}>касса</Text>
+        </View>
+      </View>
+    )
+  }
+}
+
+
+function UselessTextInput() {
+  const [text, setText] = React.useState("например: молоко");
+  return (
+    <View>
+      <TextInput
+        style={styles.find}
+        onChangeText={text => setText(text)}
+        value={text}
+      />
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: { flex: 1, padding: 0, paddingTop: 20, backgroundColor: '#fff' },
+  head: { height: 30, backgroundColor: '#808B97' },
+  text: { margin: 6 },
+  row: { flexDirection: 'row', backgroundColor: '#FFF1C1' },
+  btn: { flex: 0, height: 40, backgroundColor: '#78B7BB',  borderRadius: 5},
+  btnText: { fontSize: 25, textAlign: 'center', color: '#fff' },
+  total: {flex: 0, height: 40, justifyContent: 'center', alignItems: 'flex-end', backgroundColor: 'blue'},
+  totaltxt: { fontSize: 20, textAlign: 'center', color: '#fff' },
+  find: { height: 30, borderColor: "gray", borderWidth: 1, alignItems: "stretch", justifyContent: "space betwen"},
+});
