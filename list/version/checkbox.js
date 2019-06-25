effec6c6d3901988ee8c32aea87813f1e69ad572
JLarky <jlarky@gmail.com>
2019-06-25 00:12:24 -0700

    prettier

--- list/version/checkbox.js
+++ list/version/checkbox.js
@@ -1,59 +1,52 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import { CheckBox } from 'react-native-elements'
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { CheckBox } from "react-native-elements";
 
-
- export default class App extends React.Component {
+export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Hello, world!</Text>
+        <CheckBox title="Click Here" checked={true} />
+        <CheckBox center title="Click Here" checked={true} />
         <CheckBox
-        title='Click Here'
-        checked={true}
-          />
-          <CheckBox
           center
-          title='Click Here'
+          title="Click Here"
+          checkedIcon="dot-circle-o"
+          uncheckedIcon="circle-o"
           checked={true}
-          />
-          <CheckBox
-          center
-          title='Click Here'
-          checkedIcon='dot-circle-o'
-          uncheckedIcon='circle-o'
-          checked={true}
-          />
+        />
 
-          <CheckBox
+        <CheckBox
           center
-          title='Click Here to Remove This Item'
+          title="Click Here to Remove This Item"
           iconRight
-          iconType='material'
-          checkedIcon='clear'
-          uncheckedIcon='add'
-          checkedColor='red'
+          iconType="material"
+          checkedIcon="clear"
+          uncheckedIcon="add"
+          checkedColor="red"
           checked={true}
-          />
-          <CheckBox
-          checkedIcon={<Image source={require('../list/image/checked.png')} />}
-          uncheckedIcon={<Image source={require('../list/image/unchecked.png')} />}
+        />
+        <CheckBox
+          checkedIcon={<Image source={require("../list/image/checked.png")} />}
+          uncheckedIcon={
+            <Image source={require("../list/image/unchecked.png")} />
+          }
           checked={true}
-          onPress={() => this.setState({checked: !true})}
-          />
+          onPress={() => this.setState({ checked: !true })}
+        />
       </View>
-
     );
   }
 }
 
- const styles = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
 
 // Примечание: hello world взято у себя для проверки раотоспособности. добавлен только CheckBox вот от сюда https://react-native-training.github.io/react-native-elements/docs/checkbox.html#checkedtitle
--- list/version/only visual.js
+++ list/version/only visual.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   StyleSheet,
   Text,
@@ -7,7 +7,7 @@ import {
   TextInput,
   Button,
   TouchableOpacity,
-  Alert,
+  Alert
 } from "react-native";
 import {
   Table,
@@ -15,47 +15,55 @@ import {
   Row,
   Rows,
   Cell
-} from 'react-native-table-component';
+} from "react-native-table-component";
 
 export default class ExampleFour extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      tableHead: ['№', '☑', 'Товар', 'цена', 'количество', 'шт,упк,г,мл'],
+      tableHead: ["№", "☑", "Товар", "цена", "количество", "шт,упк,г,мл"],
       tableData: [
-        ['1', '☑', 'молоко', '36,9 ₽', '2', 'упк' ],
-        ['2', '☑', 'авокадо', '60 ₽', '3', 'шт' ],
-        ['3', '☑', 'Картофель', '15 ₽ ', '5', 'кг' ],
+        ["1", "☑", "молоко", "36,9 ₽", "2", "упк"],
+        ["2", "☑", "авокадо", "60 ₽", "3", "шт"],
+        ["3", "☑", "Картофель", "15 ₽ ", "5", "кг"]
       ]
-    }
+    };
   }
   render() {
-      const state = this.state;
-        <View style={styles.btn}>
-          <Text style={styles.btnText}>касса</Text>
-        </View>
-
+    const state = this.state;
+    <View style={styles.btn}>
+      <Text style={styles.btnText}>касса</Text>
+    </View>;
 
     return (
-     <View style={styles.container}>
-        <Table borderStyle={{borderColor: 'transparent'}}>
-          <Row data={state.tableHead} flexArr={[0.35, 0.35, 1, 0.75, 0.75, 0.55]} style={styles.head} textStyle={styles.text}/>
-              <TableWrapper style={styles.row} >
-          <Rows data={state.tableData} flexArr={[0.35, 0.35, 1, 0.75, 0.75, 0.55]} style={styles.row} textStyle={styles.text}/>
-              </TableWrapper>
+      <View style={styles.container}>
+        <Table borderStyle={{ borderColor: "transparent" }}>
+          <Row
+            data={state.tableHead}
+            flexArr={[0.35, 0.35, 1, 0.75, 0.75, 0.55]}
+            style={styles.head}
+            textStyle={styles.text}
+          />
+          <TableWrapper style={styles.row}>
+            <Rows
+              data={state.tableData}
+              flexArr={[0.35, 0.35, 1, 0.75, 0.75, 0.55]}
+              style={styles.row}
+              textStyle={styles.text}
+            />
+          </TableWrapper>
         </Table>
-       <UselessTextInput/>
-         <View style={styles.total}>
+        <UselessTextInput />
+        <View style={styles.total}>
           <Text style={styles.totaltxt}> итого: 328,8 ₽ </Text>
-         </View>
+        </View>
         <View style={styles.btn}>
           <Text style={styles.btnText}>касса</Text>
         </View>
       </View>
-    )
+    );
   }
 }
-
 
 function UselessTextInput() {
   const [text, setText] = React.useState("например: молоко");
@@ -70,13 +78,25 @@ function UselessTextInput() {
   );
 }
 const styles = StyleSheet.create({
-  container: { flex: 1, padding: 0, paddingTop: 20, backgroundColor: '#fff' },
-  head: { height: 30, backgroundColor: '#808B97' },
+  container: { flex: 1, padding: 0, paddingTop: 20, backgroundColor: "#fff" },
+  head: { height: 30, backgroundColor: "#808B97" },
   text: { margin: 6 },
-  row: { flexDirection: 'row', backgroundColor: '#FFF1C1' },
-  btn: { flex: 0, height: 40, backgroundColor: '#78B7BB',  borderRadius: 5},
-  btnText: { fontSize: 25, textAlign: 'center', color: '#fff' },
-  total: {flex: 0, height: 40, justifyContent: 'center', alignItems: 'flex-end', backgroundColor: 'blue'},
-  totaltxt: { fontSize: 20, textAlign: 'center', color: '#fff' },
-  find: { height: 30, borderColor: "gray", borderWidth: 1, alignItems: "stretch", justifyContent: "space betwen"},
+  row: { flexDirection: "row", backgroundColor: "#FFF1C1" },
+  btn: { flex: 0, height: 40, backgroundColor: "#78B7BB", borderRadius: 5 },
+  btnText: { fontSize: 25, textAlign: "center", color: "#fff" },
+  total: {
+    flex: 0,
+    height: 40,
+    justifyContent: "center",
+    alignItems: "flex-end",
+    backgroundColor: "blue"
+  },
+  totaltxt: { fontSize: 20, textAlign: "center", color: "#fff" },
+  find: {
+    height: 30,
+    borderColor: "gray",
+    borderWidth: 1,
+    alignItems: "stretch",
+    justifyContent: "space betwen"
+  }
 });
