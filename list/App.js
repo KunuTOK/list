e1562cf2188e3f0f844b282f0bd6c0d51d56b704
KunuTOK <kunutok@mail.ru>
2019-06-30 21:41:20 +0300

    Update App.js

--- list/App.js
+++ list/App.js
@@ -9,29 +9,35 @@ import {
   Row,
 } from "react-native-table-component";
 
-export default function App() { 
+export default class ExampleFour extends Component { 
+  constructor(props) {
+    super(props);   
   this.state = {
-  a: ["f", "f", "f"]};    
+  a: ["f", "f", "f"]};  
+  };
+}
+
+render() {  
   const [checked, setChecked] = React.useState(false);
 
-    return (
-      <View style={styles.container}>
-        <Table>
-          <Row 
-          data={this.state.a}
-          flexArr={[1, 1, 1]}
-          />
-        </Table> 
-    <CheckBox
-        checked={checked}
-        onPress={() => setChecked(!checked)}
+  return (
+    <View style={styles.container}>
+      <Table>
+        <Row 
+        data={this.state.a}
+        flexArr={[1, 1, 1]}
+        />
+      </Table> 
+      <CheckBox
+      checked={checked}
+      onPress={() => setChecked(!checked)}
       />
      <Text> Hello, world!</Text>      
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create ({
   container: {
     flex: 1,
     backgroundColor: "#fff",
